5f3731ce4a6e2b9481fa4b0e9463f0f8e568a426
martinvalinoti <martinvalinoti@gmail.com>
2023-02-10 20:39:39 -0300

    cambiosenform

--- tasks/forms.py
+++ tasks/forms.py
@@ -5,7 +5,7 @@ from .models import Task
 class TaskForm(forms.ModelForm):
     class Meta:
         model = Task
-        fields = ['title','descripcion','important']
+        fields = ['title','description','important']
         widgets= {
             'titulo': forms.TextInput(attrs={'class': 'form-control' , 'placeholder': 'Escribe un titulo'}),
             'descripción': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Escribe una decripción'}),
